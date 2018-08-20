437a74904b0e76b576aac68c0ae70631b095043a
BoulCheng <1241292849@qq.com>
2018-08-20 10:01:46 +0800

    log4j2

--- src/main/java/com/test/dbtest/thread/SelectRunnable.java
+++ src/main/java/com/test/dbtest/thread/SelectRunnable.java
@@ -3,6 +3,8 @@ package com.test.dbtest.thread;
 import com.test.dbtest.dao.domain.DBTest;
 import com.test.dbtest.service.DBTestServiceImpl;
 import com.test.dbtest.util.BeanUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.List;
  * Description
  */
 public class SelectRunnable implements Runnable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SelectRunnable.class);
 
     private static DBTestServiceImpl dbTestService = BeanUtil.getBean(DBTestServiceImpl.class);
     private Long id = 0L;
@@ -25,7 +28,7 @@ public class SelectRunnable implements Runnable {
                 id = dbTests.get(dbTests.size() - 1).getId();
 
                 for (DBTest dbTest : dbTests) {
-                    System.out.println(dbTest.getId());
+                    LOGGER.info(dbTest.getId() + "");
                 }
             }
         }
